0655e752a91175a20b30e6c3709d68a49325e6ae
Desmond <ifeco133@gmail.com>
2025-03-26 20:40:51 +0000

    exercise 1.7

--- part_1/unicafe/src/components/Statistics.jsx
+++ part_1/unicafe/src/components/Statistics.jsx
@@ -3,12 +3,25 @@ import Header from "./Header";
 import Feedback from "./Feedback";
 
 const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+
+  const handleAverage = () => {
+    return total / 3;
+  };
+
+  const positivePercentage = (good / total) * 100;
+
+  console.log({ positivePercen: good });
+
   return (
     <div>
       <h2>Statistics</h2>
       <Feedback feedback="Good" value={good} />
       <Feedback feedback="Neutral" value={neutral} />
       <Feedback feedback="Bad" value={bad} />
+      <Feedback feedback="All" value={total} />
+      <Feedback feedback="Average" value={handleAverage()} />
+      <Feedback feedback="Positive" value={`${positivePercentage || 0}%`} />
     </div>
   );
 };
