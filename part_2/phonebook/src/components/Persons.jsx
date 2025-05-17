37132999ec43e81409a65b8d7805e4489f639e7b
Ifcodes <ifeco133@gmail.com>
2025-05-17 16:34:00 +0100

    exercise 2.12 - 2.15

--- part_2/phonebook/src/components/Persons.jsx
+++ part_2/phonebook/src/components/Persons.jsx
@@ -4,9 +4,16 @@ import phonebookServices from "../phonebookServices";
 const Persons = ({ persons, setPersons }) => {
   const handleDelete = (person) => {
     if (window.confirm(`Are you sure you want to delete ${person.name}?`)) {
-      phonebookServices.remove(person.id).then((res) => {
-        setPersons(persons.filter((person) => person.id !== res.id));
-      });
+      phonebookServices
+        .remove(person.id)
+        .then((res) => {
+          setPersons(persons.filter((person) => person.id !== res.id));
+        })
+        .catch((error) => {
+          console.error("Error deleting person:", error);
+          alert(` ${person.name} no longer exist in the server`);
+          setPersons(persons.filter((p) => p.id !== person.id));
+        });
     }
   };
 
